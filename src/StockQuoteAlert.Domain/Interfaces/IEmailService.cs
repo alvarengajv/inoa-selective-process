7d7a1d40ff4d8c95888b3e9f26c476c01c639ac6
João Vitor Alvarenga <alvarengajv@proton.me>
2025-10-28 12:49:16 -0300

    feat: definicao de interface para o servico de email

---
+++ src/StockQuoteAlert.Domain/Interfaces/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuoteAlert.Domain.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendAlertAsync(string subject, string body);
+    }
+}
